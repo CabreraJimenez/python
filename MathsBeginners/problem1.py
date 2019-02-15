2c288c452c873f5d3e1878ca096b7a4647068eaf
Carlos Cabrera <43237616+CabreraJimenez@users.noreply.github.com>
2019-02-15 17:12:03 +0100

    Update problem1.py

--- MathsBeginners/problem1.py
+++ MathsBeginners/problem1.py
@@ -6,15 +6,13 @@ Created on Fri Feb 15 15:06:31 2019
 """
 
 """If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 #We are going to resolve this problems for any number:
-
 number = input('Find the sum of all the multiples of 3 or 5 below: ' )
 number = int(number)
-#The rest is the best option 
 
+#The rest is the best option 
 def function(num):
     i=1
     sum=0
