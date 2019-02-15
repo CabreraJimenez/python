8be423f3c54436a9a152db4244dd74724b49e926
Carlos Cabrera <43237616+CabreraJimenez@users.noreply.github.com>
2019-02-15 17:10:17 +0100

    Update problem2.py

--- MathsBeginners/problem2.py
+++ MathsBeginners/problem2.py
@@ -13,19 +13,15 @@ max = input("maximum value of Fibonacci serie: (greater than 1) ")
 max = int(max)
 
 #We have to know that the Fibonacci is: 1,1,2,3,5,8,13,21,34...
-
 def function(max):
     num1 = 1
     num2 = 1
-    
     sum = 1
     while num2 <= max:
         sum = sum + num2
         aux = num2 #it is necessary because we want change two values that depends each other at the same time
         num2 = num1 + num2
         num1 = aux
-
-        
     return sum
 
 print('The result is: ' + str(function(max)))
